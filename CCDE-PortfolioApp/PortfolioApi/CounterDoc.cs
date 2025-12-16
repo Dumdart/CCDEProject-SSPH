6b6baf833e92bd75ebd13816034da736ffc9639d
Paul Thumfart <paul.thumfart@gmail.com>
2025-12-16 14:43:45 +0100

    Adjusted Gitignore for VisualStudio, Add Function and configure it simply

---
+++ CCDE-PortfolioApp/PortfolioApi/CounterDoc.cs
@@ -0,0 +1,16 @@
+﻿namespace PortfolioApi;
+
+public record CounterDoc {
+    public CounterDoc(string id, string pageId, int viewCount, string? lastUpdated) {
+        this.id = id;
+        this.pageId = pageId;
+        this.viewCount = viewCount;
+        this.lastUpdated = lastUpdated;
+    }
+
+    public string id { get; init; }
+    public string pageId { get; init; }
+    public int viewCount { get; init; }
+    public string? lastUpdated { get; init; }
+
+}
